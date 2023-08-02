d021e9245998590b2fb9491a16bfa88530abb455
aleshaeslichto <i@aleshaeslichto.ru>
2023-08-02 10:02:48 +0300

    update task.js v.0.2

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,18 +1,46 @@
+// const rotatorCases = document.querySelectorAll('.rotator__case');
+
+// let activeIndex = 0;
+// let speed = +rotatorCases[0].dataset.speed;
+
+// setInterval(() => {
+//     color = rotatorCases[activeIndex].dataset.color; // получаем значение цвета
+//     speed = +rotatorCases[activeIndex].dataset.speed; // получаем значение задержки
+
+//     rotatorCases[activeIndex].style.color = color; // устанавливаем цвет
+
+//     rotatorCases[activeIndex].classList.remove('rotator__case_active');
+
+//     activeIndex++;
+//     activeIndex = activeIndex >= rotatorCases.length ? 0 : activeIndex;
+
+//     rotatorCases[activeIndex].classList.add('rotator__case_active');
+// }, speed);
+
 const rotatorCases = document.querySelectorAll('.rotator__case');
 
 let activeIndex = 0;
-let speed = +rotatorCases[0].dataset.speed;
 
-setInterval(() => {
-    color = rotatorCases[activeIndex].dataset.color; // получаем значение цвета
-    speed = +rotatorCases[activeIndex].dataset.speed; // получаем значение задержки
+function rotate() {
 
-    rotatorCases[activeIndex].style.color = color; // устанавливаем цвет
+  const currentCase = rotatorCases[activeIndex];
 
-    rotatorCases[activeIndex].classList.remove('rotator__case_active');
+  const color = currentCase.dataset.color;
+  const speed = +currentCase.dataset.speed;
 
-    activeIndex++;
-    activeIndex = activeIndex >= rotatorCases.length ? 0 : activeIndex;
+  currentCase.style.color = color;
 
-    rotatorCases[activeIndex].classList.add('rotator__case_active');
-}, speed);
+  currentCase.classList.remove('rotator__case_active');
+
+  activeIndex++;
+  if (activeIndex >= rotatorCases.length) {
+    activeIndex = 0;
+  }
+
+  const nextCase = rotatorCases[activeIndex];
+  nextCase.classList.add('rotator__case_active');
+
+  setTimeout(rotate, speed);
+}
+
+setTimeout(rotate, 1000);
